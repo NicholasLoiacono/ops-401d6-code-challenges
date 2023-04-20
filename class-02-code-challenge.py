66491d601be4b11427aa98b0aac7d9892b347fca
NicholasLoiacono <ntloiacono4@gmail.com>
2023-04-20 06:33:21 +0000

    Prep

---
+++ class-02-code-challenge.py
@@ -0,0 +1,11 @@
+# Shebang line to specify the interpreter to use for running the script.
+#!/usr/bin/python3
+
+# Script: Ops 401d6 Class 02 Ops Challenge Solution
+# Author: Nicholas Loiacono
+# Date: 04/18/2023
+# Purpose: Uptime Sensor Tool Part 1 of 2
+# Resources: Chat GPT
+
+
+# Start
---
+++ class-03-code-challenge.py
@@ -0,0 +1,11 @@
+# Shebang line to specify the interpreter to use for running the script.
+#!/usr/bin/python3
+
+# Script: Ops 401d6 Class 03 Ops Challenge Solution
+# Author: Nicholas Loiacono
+# Date: 04/19/2023
+# Purpose: Uptime Sensor Tool Part 2 of 2
+# Resources: Chat GPT
+
+
+# Start
