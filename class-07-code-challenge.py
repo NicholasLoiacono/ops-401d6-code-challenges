e6b04bebb36a803912a1e5f439a1a448f17c5a85
Nicholas Loiacono <105306699+NicholasLoiacono@users.noreply.github.com>
2023-04-25 14:41:22 -0700

    Code Prep

---
+++ class-07-code-challenge.py
@@ -0,0 +1,11 @@
+# Shebang line to specify the interpreter to use for running the script.
+#!/usr/bin/python3
+
+# Script: Ops 401d6 Class 07 Ops Challenge Solution
+# Author: Nicholas Loiacono
+# Date: 04/25/2023
+# Purpose: File Encryption Script Part 2 of 3
+# Resources: Chat GPT
+
+
+# Start
