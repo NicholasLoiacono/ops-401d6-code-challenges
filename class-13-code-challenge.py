cd077c9c2530c2fb3683b2ac566af39d5aade591
Nicholas Loiacono <NTLoiacono4@gmail.com>
2023-05-04 05:50:06 +0000

    Code Prep

---
+++ class-13-code-challenge.py
@@ -0,0 +1,11 @@
+# Shebang line to specify the interpreter to use for running the script.
+#!/usr/bin/python3
+
+# Script: Ops 401d6 Class 13 Ops Challenge Solution
+# Author: Nicholas Loiacono
+# Date: 05/03/2023
+# Purpose: Network Security Tool with Scapy Part 3 of 3
+# Resources: Chat GPT
+
+
+# Start
