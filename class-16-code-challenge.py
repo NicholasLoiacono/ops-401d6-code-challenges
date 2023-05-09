3f9099e72608cace2fa6e060c45428f2c36e0c68
Nicholas Loiacono <NTLoiacono4@gmail.com>
2023-05-09 06:51:13 +0000

    Code Prep

---
+++ class-16-code-challenge.py
@@ -0,0 +1,11 @@
+# Shebang line to specify the interpreter to use for running the script.
+#!/usr/bin/python3
+
+# Script: Ops 401d6 Class 16 Ops Challenge Solution
+# Author: Nicholas Loiacono
+# Date: 05/08/2023
+# Purpose: Automated Brute Force Wordlist Attack Tool Part 1 of 3
+# Resources: Chat GPT
+
+
+# Start
