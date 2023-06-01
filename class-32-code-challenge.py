86d203338a9b10333fb259313f41970b968c23b6
Nicholas Loiacono <NTLoiacono4@gmail.com>
2023-06-01 20:16:26 +0000

    Signature-based Malware Detection Part 2 of 3

---
+++ class-32-code-challenge.py
@@ -0,0 +1,60 @@
+# Shebang line to specify the interpreter to use for running the script.
+#!/usr/bin/python3
+
+# Script: Ops 401d6 Class 32 Ops Challenge Solution
+# Author: Nicholas Loiacono
+# Date: 05/31/2023
+# Purpose: Signature-based Malware Detection Part 2 of 3
+# Resources: Chat GPT
+
+
+# Start
+
+
+import os
+import hashlib
+import time
+
+def get_file_info(file_path):
+    with open(file_path, 'rb') as file:
+        data = file.read()
+        md5_hash = hashlib.md5(data).hexdigest()
+
+    file_info = {
+        "file_name": os.path.basename(file_path),
+        "file_path": os.path.abspath(file_path),
+        "file_size": os.path.getsize(file_path),
+        "md5_hash": md5_hash,
+        "timestamp": time.ctime(os.path.getmtime(file_path))
+    }
+    
+    return file_info
+
+def scan_directory(search_path):
+    for root, dirs, files in os.walk(search_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_info = get_file_info(file_path)
+            print_file_info(file_info)
+
+def print_file_info(file_info):
+    print(f"\nTimestamp: {file_info['timestamp']}")
+    print(f"File Name: {file_info['file_name']}")
+    print(f"File Size: {file_info['file_size']} bytes")
+    print(f"File Path: {file_info['file_path']}")
+    print(f"MD5 Hash: {file_info['md5_hash']}")
+
+def main():
+    search_path = input("Enter the directory to search in: ")
+    
+    if not os.path.isdir(search_path):
+        print("The provided path is not a valid directory.")
+        return
+
+    scan_directory(search_path)
+
+if __name__ == "__main__":
+    main()
+
+
+# End
